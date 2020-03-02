2eafb1b553dbbb4c32748f849e033cffd4c05944
henrikasvand <henri.kasvand@gmail.com>
2020-03-02 14:39:38 +0200

    game of quotes that is actually full-stack

---
+++ quoteserveri/public/build/precache-manifest.23957f3b08a58d95abdce2090f4f16e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73eaee8c9419fbca9204310df1806c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d05fd8fae63c95dc38",
+    "url": "/static/css/main.1389705e.chunk.css"
+  },
+  {
+    "revision": "3cdd7a841585cf45aae2",
+    "url": "/static/js/2.92f27f1f.chunk.js"
+  },
+  {
+    "revision": "933f139c1acbc3546cc823b3421e06f7",
+    "url": "/static/js/2.92f27f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d05fd8fae63c95dc38",
+    "url": "/static/js/main.0ebef601.chunk.js"
+  },
+  {
+    "revision": "803ac90ba3531b1497a0",
+    "url": "/static/js/runtime-main.00e782b2.js"
+  },
+  {
+    "revision": "1ea74a1a2598f8ac8614f2d8fb9fb3b3",
+    "url": "/static/media/Throneback.1ea74a1a.jpg"
+  },
+  {
+    "revision": "439b9e6c8e111b7c3a1b95882c4d3152",
+    "url": "/static/media/dragond.439b9e6c.png"
+  }
+]);
